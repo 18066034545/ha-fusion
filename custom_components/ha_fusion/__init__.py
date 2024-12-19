b3aa51506ca37cd8f7c3c753a479a29c62652fbe
Intel <303316404@qq.com>
2024-12-19 13:21:50 +0800

    feat: 添加HACS UI配置支持

--- custom_components/ha_fusion/__init__.py
+++ custom_components/ha_fusion/__init__.py
@@ -1,1 +1,10 @@
- 
+"""HA Fusion integration."""
+DOMAIN = "ha_fusion"
+
+async def async_setup(hass, config):
+    """Set up this integration using YAML."""
+    return True
+
+async def async_setup_entry(hass, config_entry):
+    """Set up this integration using UI."""
+    return True 
---
+++ custom_components/ha_fusion/const.py
@@ -0,0 +1,5 @@
+"""Constants for the HA Fusion integration."""
+
+DOMAIN = "ha_fusion"
+NAME = "HA Fusion"
+VERSION = "2024.12.0" 
